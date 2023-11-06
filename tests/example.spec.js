8ad6ff9fe5f0ea0db2503c4af3bfde5e2df1d52c
Gabriel Trojanowski <gabetrojanowski@outlook.com>
2023-11-06 13:17:45 -0500

    12 more playwright testd

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -32,3 +32,45 @@ test('Check Page Title', async ({ page }) => {
   expect(title).toBe(expectedTitle);
 });
 
+
+test('Check Main Header Presence', async ({ page }) => {
+  await expect(page.locator('header.header')).toBeVisible();
+});
+
+test('Check GitHub Link', async ({ page }) => {
+  await expect(page.locator('a[href="https://www.github.com/gabrielt01/"]')).toHaveAttribute('href', 'https://www.github.com/gabrielt01/');
+});
+
+test('Check LinkedIn Link', async ({ page }) => {
+  await expect(page.locator('a[href="https://www.linkedin.com/in/gabriel-trojanowski/"]')).toHaveAttribute('href', 'https://www.linkedin.com/in/gabriel-trojanowski/');
+});
+
+test('Check Profile Picture', async ({ page }) => {
+  await expect(page.locator('img[alt=""]')).toHaveAttribute('src', './images/avatar.png');
+});
+
+test('Check First Project Link', async ({ page }) => {
+  await expect(page.locator('a[href="skills.html#project-results"]:has-text("Explore to Learn More")')).toHaveAttribute('href', 'skills.html#project-results');
+});
+
+test('Check Footer Logo Link', async ({ page }) => {
+  await page.click('a.footer__inner-logo');
+  await expect(page).toHaveURL(websiteURL);
+});
+
+test('Check All Sections Exist', async ({ page }) => {
+  await expect(page.locator('section')).toHaveCount(3);
+});
+
+test('Check Analyze Icon', async ({ page }) => {
+  await expect(page.locator('img[alt=""][src="./images/icons/analyze.png"]')).toBeVisible();
+});
+
+test('Check Optimize Icon', async ({ page }) => {
+  await expect(page.locator('img[alt=""][src="./images/icons/optimize.png"]')).toBeVisible();
+});
+
+test('Check Intro Section Text', async ({ page }) => {
+  await expect(page.locator('div.intro__inner-title h2')).toContainText('Proficiently exploring and solving procurement challenges with technology');
+});
+
