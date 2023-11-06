c747a4f804254ee70773822c9200291e6067f75c
Gabriel Trojanowski <gabetrojanowski@outlook.com>
2023-11-06 01:51:20 -0500

    Check if live website works

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -19,7 +19,7 @@ test('get started link', async ({ page }) => {
 });
 
 
-const websiteURL = 'http://127.0.0.1:5500/docs/index.html';
+const websiteURL = 'https://njit-wis.github.io/midterm-portfolio-gabrielt01/index.html';
 const expectedTitle = 'Home';
 
 
