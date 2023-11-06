586bf6579d42f970ef1913e7c8cab231dd96bc5b
Gabriel Trojanowski <gabetrojanowski@outlook.com>
2023-11-05 23:18:26 -0500

    update 10000

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -18,27 +18,16 @@ test('get started link', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
 
-test.describe('Homepage tests', () => {
+const websiteURL = 'http://localhost:3000';
+const expectedTitle = 'Home'
 
-  // Navigate to the homepage before each test
-  test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:8080/'); // Replace with your actual URL
-  });
+test.beforeEach(async ({ page }) => {
+  await page.goto(websiteURL);
+});
 
-  // 1. Test that the homepage loads with the correct title
-  test('homepage has the correct title', async ({ page }) => {
-    await expect(page).toHaveTitle('Home');
-  });
+test('Check Page Title', async ({ page }) => {
+  const title = await page.title();
+  expect(title).toBe(expectedTitle);
+});
 
-  // 2. Test the visibility of the main header
-  test('main header is visible', async ({ page }) => {
-    const header = await page.locator('header.header');
-    await expect(header).toBeVisible();
-  });
 
-  // 3. Test the navigation links in the header
-  test('navigation links are correct', async ({ page }) => {
-    await expect(page.locator('a[href="skills.html#project-results"]')).toHaveText('Experience');
-    await expect(page.locator('a[href="education.html"]')).toHaveText('Education');
-    // Add tests for all navigation links
-  });
